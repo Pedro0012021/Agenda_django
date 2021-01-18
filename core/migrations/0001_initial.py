f57db3e6ab67dedbb87e5ae8bb33495a2ece7c71
pedro0012021 <pedro96629770@live.com>
2021-01-18 13:36:30 -0300

    atualizações agenda

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
                 ('titulo', models.CharField(max_length=100)),
                 ('descricao', models.TextField(blank=True, null=True)),
                 ('data_evento', models.DateTimeField()),
+                
                 ('data_criacao', models.DateTimeField(auto_now=True)),
             ],
             options={
