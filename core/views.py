aa02c54142cc84d0baa19895a6ffc7bffa7ff646
pedro0012021 <pedro96629770@live.com>
2021-01-28 13:45:45 -0300

    incluido templatespara inserçao de dados na agenda e incluido git ignore para ignorar arquivos compilados.

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.http import request
 from core.models import Evento
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import REDIRECT_FIELD_NAME, authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
@@ -43,3 +43,23 @@ def lista_eventos(request):
     #evento=Evento.objects.all
     dados={'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/') 
+def submit_evento(request):
+    if request.POST:  
+        titulo=request.POST.get('titulo')
+        data_evento=request.POST.get('data_evento')
+        descricao=request.POST.get('descricao')
+        usuario=request.user
+        Evento.objects.create(titulo=titulo,
+                            data_evento=data_evento,
+                            descricao=descricao,
+                            usuario=usuario)
+    return redirect('/')                    
+
+        
